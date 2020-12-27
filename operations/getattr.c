49d07abeb53edc5052d86949cd2bd346918a1bc8
JustBeYou <mihailferaru2000@gmail.com>
2020-12-27 22:36:56 +0200

    Add symbolic constant

--- operations/getattr.c
+++ operations/getattr.c
@@ -6,6 +6,7 @@
 #include <zip.h>
 #include <fcntl.h>
 #include <stddef.h>
+#include <string.h>
 
 #define DEFAULT_DIR_PERM 0755
 #define DEFAULT_FILE_PERM 0755
@@ -16,7 +17,7 @@ int zipfs_getattr(const char *path, struct stat *stbuf, struct fuse_file_info *f
     zip_stat_t result;
     const char* real_path = zip_path_format(path);
 
-    if (real_path[0] == '\0' || zip_is_dir(real_path, data->zip_file)) {
+    if (strcmp(real_path, ZIP_ROOT) == 0 || zip_is_dir(real_path, data->zip_file)) {
         stbuf->st_mode = S_IFDIR | DEFAULT_DIR_PERM;
         stbuf->st_nlink = 2;
     } else {
