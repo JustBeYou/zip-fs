b04574764d57167ac88e9cfa4374029ca1d9f4e3
seras3 <adrian_etto@yahoo.com>
2020-12-28 12:11:10 +0200

    error init

--- operations/init.c
+++ operations/init.c
@@ -8,15 +8,17 @@
 #include <stdlib.h>
 #include <assert.h>
 
-void* zipfs_init(struct fuse_conn_info* conn, struct fuse_config* cfg) {
-    (void) conn;
+void *zipfs_init(struct fuse_conn_info *conn, struct fuse_config *cfg)
+{
+    (void)conn;
 
     debug(printf("[DEBUG] Init filesystem for file %s.\n", zipfs_options_get()->zip_filename));
 
-    int error;
-    zipfs_data_t* data = malloc(sizeof(zipfs_data_t));
+    int error = 0;
+    zipfs_data_t *data = malloc(sizeof(zipfs_data_t));
     data->zip_file = zip_open(zipfs_options_get()->zip_filename, 0, &error);
-    if (error != ZIP_ER_OK || data->zip_file == NULL) {
+    if (error != ZIP_ER_OK || data->zip_file == NULL)
+    {
         debug(printf("[DEBUG] Failed to open archive. (%d %p)\n", error, data->zip_file));
         free(data);
         exit(1);
